e1d57ab02bb61bd9087e314ebd827e1b99468f12
vamshithecrack <vamshithecrack@gmail.com>
2016-09-26 22:38:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,11 @@ element.innerHTML = 'New value';
 
 //move the image
 var img = document.getElementById('madi');
+var marginleft = 0;
+function moveRight(){
+    marginleft=marginleft+10;
+    img.style.marginleft = marginleft + 'px';
+}
 img.onclick = function() {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight,100);
 };
