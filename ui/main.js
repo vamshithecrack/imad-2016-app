a72d3cd39c775cd0fd50c6cdc9ccac3ccd03a612
vamshithecrack <vamshithecrack@gmail.com>
2016-09-26 20:10:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,11 @@
 console.log('Loaded!');
 
+//change the text of main-text div
 var element = document.getElementById('main-text');
-
 element.innerHTML = 'New value';
+
+//move the image
+var img = document.getElementById('madi');
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+};
